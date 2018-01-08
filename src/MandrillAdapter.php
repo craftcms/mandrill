5991fb8fbdfebc83b63fa62b6beb783f63e92ce3
Brandon Kelly <brandon@pixelandtonic.com>
2018-01-08 07:09:53 -0800

    If subaccount is an empty string, treat as null

--- src/MandrillAdapter.php
+++ src/MandrillAdapter.php
@@ -94,7 +94,7 @@ class MandrillAdapter extends BaseTransportAdapter
                 ]
             ],
             'apiKey' => $this->apiKey,
-            'subAccount' => $this->subaccount,
+            'subAccount' => $this->subaccount ?: null,
         ];
     }
 }
